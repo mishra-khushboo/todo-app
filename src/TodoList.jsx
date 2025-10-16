699c20cb1db7848b7d87deb6694732e696837594
mishra-khushboo <khush27093@gmail.com>
2025-10-16 21:12:56 +0530

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,7 @@
+export default function TodoList() {
+    return (<div>
+        <h4>Todo List</h4>
+        <input placeholder="add a task"></input>
+        <button>Add Task</button>
+    </div>)
+}
