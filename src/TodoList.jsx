6fc78f10ff60e2f8d6cf8e9e1aeb642817f61d46
mishra-khushboo <khush27093@gmail.com>
2025-10-18 14:43:59 +0530

    basic todo id done

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
 export default function TodoList() {
+    let [todos, setTodos] = useState(["sample task"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos([...todos, newTodo]);
+        setNewTodo("");
+    }
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+        
+    }
+
     return (<div>
-        <h4>Todo List</h4>
-        <input placeholder="add a task"></input>
-        <button>Add Task</button>
+        <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
+        <br></br>
+        <br></br>
+        <button onClick={addNewTask}>Add Task</button>
+        <br></br> <br></br><br></br>
+        <hr></hr>
+        <h4>Tasks Todo</h4>
+        <ul>{
+            todos.map((todo) => {
+               return  <li>{todo}</li>
+            })
+        }</ul>
+
+
     </div>)
 }
